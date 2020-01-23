8d8fffe229b0bb9c62b385731c254d7dab4f16ab
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-01-23 16:35:46 -0600

    Fix another test

--- test/integration/channel_test.exs
+++ test/integration/channel_test.exs
@@ -3,9 +3,21 @@ defmodule Enmity.ChannelTest do
   alias Enmity.Channel
 
   test "get" do
-    {:ok, channel} = Channel.get(668_217_932_211_421_242)
+    {:ok, channel} = Channel.get("668217932211421242")
 
-    assert channel == "hi :)"
+    assert channel == %{
+      bitrate: 64000,
+      guild_id: "668217932211421234",
+      id: "668217932211421242",
+      name: "General",
+      nsfw: false,
+      parent_id: "668217932211421238",
+      permission_overwrites: [],
+      position: 0,
+      type: 2,
+      user_limit: 0
+    }
+
   end
 
 end
