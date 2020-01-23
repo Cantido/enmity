2000fcf3fb56362fc6b54121309c00f0067e34d4
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-01-23 17:09:35 -0600

    Split out a websocket layer for the gateway

--- lib/enmity/gateway.ex
+++ lib/enmity/gateway.ex
@@ -25,81 +25,26 @@ defmodule Enmity.Gateway do
 
   defmacro __using__(_opts) do
     quote do
-      use GenServer
+      use Enmity.Gateway.Websocket
+      alias Enmity.Gateway.Websocket
       alias Enmity.Gateway.Operations
-      alias Enmity.Gateway
       require Logger
 
-      def start_link(_args) do
-        GenServer.start_link(__MODULE__, :ok)
-      end
+      @behaviour Enmity.Gateway
 
       def connected?(pid) do
         GenServer.call(pid, :is_connected)
-      end
-
-      def init(:ok) do
-        {:ok, %{
-          connected: false,
-          last_sequence_number: nil,
-          user_state: %{}
-        }, 0}
       end
 
       def handle_call(:is_connected, _from, state = %{connected: connected}) do
         {:reply, connected, state}
       end
 
-      def handle_info(:timeout, state) do
-        {:ok, %{url: url}} = Enmity.HTTP.get("/gateway/bot")
-        parsed_url = URI.parse(url)
-        {:ok, conn_pid} = :gun.open(to_charlist(parsed_url.host), 443, %{protocols: [:http]})
-        {:ok, :http} = :gun.await_up(conn_pid)
-
-        stream_ref = :gun.ws_upgrade(
-          conn_pid,
-          '/?encoding=etf&v=6')
-
-        state = state
-        |> Map.put(:conn, conn_pid)
-        |> Map.put(:stream_ref, stream_ref)
-
-        {:noreply, state}
-      end
-
       def handle_info(:heartbeat, state = %{conn: conn_pid, last_sequence_number: seq, heartbeat_interval_ms: heartbeat_interval_ms}) do
         Logger.debug("Sending heartbeat message")
-        Gateway.send(conn_pid, Operations.heartbeat(seq))
+        Websocket.send(conn_pid, Operations.heartbeat(seq))
         Process.send_after(self(), :heartbeat, heartbeat_interval_ms)
         {:noreply, state}
-      end
-
-      def handle_info({:gun_ws, _ConnPid, _StreamRef, {:binary, frame}}, state) do
-        body = Gateway.decode_frame(frame)
-
-        Logger.debug("Recieved websocket message #{inspect body}")
-
-        handle_operation(body, state)
-      end
-
-      def handle_info({:gun_upgrade, _conn_pid, _stream_ref, _, _}, state) do
-        Logger.debug("Successfully upgraded to a websocket connection")
-        {:noreply, state}
-      end
-
-      def handle_info(msg, state) do
-        Logger.debug("Got an unrecognized message: #{inspect msg}")
-        {:noreply, state}
-      end
-
-      def handle_event(_, _, state) do
-        {:ok, state}
-      end
-
-      def terminate(reason, state) do
-        if Map.has_key?(state, :conn_pid) do
-          :gun.shutdown(state.conn_pid)
-        end
       end
 
       def handle_operation(body, state = %{conn: conn_pid}) do
@@ -132,7 +77,7 @@ defmodule Enmity.Gateway do
               |> Map.get(:last_sequence_number)
               |> Operations.identify()
 
-            Gateway.send(conn_pid, payload)
+            Websocket.send(conn_pid, payload)
             {:noreply, Map.put(state, :heartbeat_interval_ms, heartbeat_interval_ms)}
           # heartbeat ack
           11 ->
@@ -140,6 +85,10 @@ defmodule Enmity.Gateway do
           _ ->
             {:noreply, state}
         end
+      end
+
+      def handle_event(_, _, state) do
+        {:ok, state}
       end
     end
   end
@@ -162,15 +111,4 @@ defmodule Enmity.Gateway do
       end
   """
   @callback handle_event(term(), term(), term()) :: {:ok, term()}
-
-  def decode_frame(frame) do
-    frame
-    |> :erlang.iolist_to_binary()
-    |> :erlang.binary_to_term()
-  end
-
-  def send(conn_pid, payload) do
-    payload = :erlang.term_to_binary(payload)
-    :gun.ws_send(conn_pid, {:binary, payload})
-  end
 end
